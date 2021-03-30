d62d4fc5c18be5c9c835f1dffe9a622d41b6758d
RenatoZangerolami <renato.zanger@gmail.com>
2021-03-30 15:41:59 -0300

    Commit final(map,mutable)

---
+++ src/main/kotlin/one/digitalinnovation/collections/testeColecoesMutaveis.kt
@@ -0,0 +1,32 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val joao = Funcionario("Joao", 2000.0, "CLT")
+    val pedro = Funcionario("pedro", 1500.0, "PJ")
+    val maria = Funcionario("maria", 4000.0, "CLT")
+
+    println("-------LIST---------")
+    val funcionarios = mutableListOf(joao, maria)
+    funcionarios.forEach { println(it) }
+
+    println("----------------")
+    funcionarios.add(pedro)
+    funcionarios.forEach { println(it) }
+
+    println("----------------")
+    funcionarios.remove(joao)
+    funcionarios.forEach { println(it) }
+
+    println("------SET----------")
+    val funcionarioSet = mutableSetOf(joao)
+    funcionarioSet.forEach { println(it) }
+
+    println("------SET----------")
+    funcionarioSet.add(pedro)
+    funcionarioSet.add(maria)
+    funcionarioSet.forEach { println(it) }
+
+    println("----------------")
+    funcionarioSet.remove(maria)
+    funcionarioSet.forEach { println(it) }
+}
