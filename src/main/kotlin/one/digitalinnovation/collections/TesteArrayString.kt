bafe7d1589cbd095138615d63b5db3feef8cd7bd
RenatoZangerolami <renato.zanger@gmail.com>
2021-03-30 11:09:12 -0300

    Criacao de Arrays

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteArrayString.kt
@@ -0,0 +1,24 @@
+package one.digitalinnovation.collections
+
+fun main(){
+    //Precisa colocar o tipo, mas como foi inicializado com Espacos, ja foi reconhecido como String
+    val nomes = Array(3){""}
+    nomes[0]= "Maria"
+    nomes[1]= "Zaza"
+    nomes[2]= "Jose"
+
+    for (nome in nomes){
+        println(nome)
+    }
+
+    println("-------")
+    nomes.sort()
+    nomes.forEach { println(it) }
+
+    println("-------")
+    val nomes2 = arrayOf("Maria","Zaza","Pedro")
+    nomes2.sort()
+    nomes2.forEach { println(it) }
+
+
+}
